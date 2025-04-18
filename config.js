a50ea5958181ff230b0503a30457dabc5d968152
kopralwann <136798833+kopralwann@users.noreply.github.com>
2025-04-19 05:30:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  TOKEN: 'BOT_TOKEN',
+  TOKEN: '7451826170:AAFuUF9vyjZQGcHoA5gx89C8wTWIkeseZcw',
   SELLER_CODE: 'SELLER_KODE_KMSP',
-  OWNER_NAME: 'Sonzai X シ',
-  OWNER_USERNAME: 'November2k',
-  OWNER_ID: 'OWNER_ID',
+  OWNER_NAME: 'Bang Kopral',
+  OWNER_USERNAME: 'Bangkopral',
+  OWNER_ID: '6904837223',
   MONGO_DB: 'MONGO_DB_URL',
   DISCLAIMER_LINK: 'https://t.me/November2kLab/4'
 };
